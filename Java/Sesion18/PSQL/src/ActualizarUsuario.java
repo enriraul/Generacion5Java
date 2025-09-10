938e77c724dd31e42fef9c1076a6eae8fd015b99
Enrique Raúl OR <128866748+enriraul@users.noreply.github.com>
2025-09-10 10:07:58 -0600

    Add files via upload

---
+++ Java/Sesion18/PSQL/src/ActualizarUsuario.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+
+public class ActualizarUsuario{
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/dbtest";
+        String usuario = "admin";
+        String password = "root";
+        try {
+            Class.forName("org.postgresql.Driver");
+            try(Connection conn = DriverManager.getConnection(url, usuario, password)){
+                String update = "UPDATE usuario SET correo = ? WHERE id_usuario = ?";
+                //"UPDATE usuario SET correo = correonuevo@ejemplo.com.mx, nombre = pedro WHERE id_usuario = 1"
+                //"UPDATE usuario SET correo = corre2onuevo@ejemplo.com.mx WHERE id_usuario = 2"
+                try(PreparedStatement pstmt  = conn.prepareStatement(update)){
+                    pstmt.setString(1,"correonuevo@ejemplo.com.mx");
+                    pstmt.setInt(2,1);
+                    pstmt.setString(1,"correo2nuevo@ejemplo.com.mx");
+                    pstmt.setInt(2,2);
+                    int filas = pstmt.executeUpdate();
+                    System.out.println("Numero de actualizaciones: "+filas);
+                }
+            
+            }catch(SQLException e){
+                System.out.println("Error: "+e.getMessage());
+            }
+        }catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+        /*
+        
+        
+        
+        */
+    }
+}
---
+++ Java/Sesion18/PSQL/src/InsertarUsuario.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class InsertarUsuario{
+
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/dbtest";
+        String usuario = "admin";
+        String password = "root";
+        try {
+            Class.forName("org.postgresql.Driver");
+            try(Connection conn = DriverManager.getConnection(url, usuario, password)){
+                String insert = "INSERT INTO usuario (nombre, apellido, correo) VALUES (?,?,?)";
+                try(PreparedStatement pstmt  = conn.prepareStatement(insert)){
+                    pstmt.setString(1,"Enrique");
+                    pstmt.setString(3, "correo@ejemplo.com");
+                    pstmt.setString(2, "Rodríguez");
+
+                    int filas = pstmt.executeUpdate();
+                    System.out.println("Numero de actualizaciones: "+filas);
+                }
+
+            }
+            catch(SQLException e){
+            System.out.println("Error del mensaje: "+e.getMessage());
+            }
+        } 
+        catch(Exception e){
+            System.out.println("Error del mensaje: "+e.getMessage());
+        }
+
+    }
+
+}
